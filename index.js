ed7da7036733f057366c64a122f4f95727c5e208
mbron770 <mbronfin.tsd@gmail.com>
2023-05-29 00:56:58 -0400

    add functions

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+
+function writeCards(names, event) {
+    const newNames = [];
+    for(let i =0; i < names.length; i++){
+      const message = (`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+      newNames.push(message)
+    }
+
+    return newNames 
+
+}
+
+
+function countDown() {
+    let count = 10;
+    while(0 <= count) {
+        console.log(count--); 
+    }
+}
